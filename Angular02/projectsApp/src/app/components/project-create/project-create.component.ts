6046dee369d72cd3bdbbe0b3333681e285d686b7
Efra <efrain_serna@hotmail.com>
2021-06-24 13:29:54 -0500

    create project

--- Angular02/projectsApp/src/app/components/project-create/project-create.component.ts
+++ Angular02/projectsApp/src/app/components/project-create/project-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/project';
 
 @Component({
   selector: 'prj-project-create',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-create.component.css']
 })
 export class ProjectCreateComponent implements OnInit {
+  pageTitle:string = 'Create new project';
+  _project!: Project;
+  get NewProject():Project{
+    return this._project;
+  }
+  set NewProject(value: Project) {
+    this._project = value;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    this._project = this.new();
   }
 
+  new():Project {
+    return {
+      ProjectId: 0,
+      "Name": "",
+      TypeId: 0,
+      Ranking: 0,
+      LastModified: ""
+    };
+  }
+  save():void {
+    console.log(this._project);
+    this._project = this.new();
+
+  }
 }
